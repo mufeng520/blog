1de4af0a62b67e9fd0d77f1d1fdb95ac6404e40f
mufeng520 <2018998107@qq.com>
2026-02-26 10:42:29 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,5 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function GET(context) {
-  // 获取所有文章
-  const blog = await getCollection('blog');
   return rss({
     // RSS Feed 标题
     title: '木风的博客',
@@ -11,14 +8,9 @@ export async function GET(context) {
     // 网站地址
     site: context.site || 'https://rita.cc.cd',
     // 文章列表
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      // 从 `id` 属性计算出 RSS 链接
-      // 这个例子假设所有的文章都被渲染为 `/blog/[id]` 路由
-      link: `/posts/${post.id}/`,
-    })),
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./posts/*.md', { eager: true }),
+    ),
     // 自定义命名空间
     customData: `<language>zh-CN</language>`,
   });
